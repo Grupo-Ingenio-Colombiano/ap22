8ef05d36aab789da2828775338a8e958fa94cde0
FelipeIzq28 <felipeizquierdo47@gmail.com>
2023-04-18 16:09:21 -0500

    Fix first dialogue
    Fix first dialogue with plant boss in load

--- Assets/Scripts/Load/LoadCharacter.cs
+++ Assets/Scripts/Load/LoadCharacter.cs
@@ -8,8 +8,22 @@ public class LoadCharacter : MonoBehaviour
     [SerializeField] UserData userData;
     [SerializeField] GameObject dialogSST;
 
+    [SerializeField]
+    VIDE_Assign videJefe;
+
     // Update is called once per frame
-   
+
+    private void Start()
+    {
+        if(userData.isSave == true)
+        {
+            if(userData.load >= 1)
+            {
+                videJefe.overrideStartNode = 10;
+            }
+        }
+    }
+
     public void SetIndicatorLoad()
     {
         if(userData.load == 1)
