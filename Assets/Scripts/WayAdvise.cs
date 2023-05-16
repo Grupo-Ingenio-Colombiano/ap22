8b8ee48fcd092798ad9c099a30582626effe93de
FelipeIzq28 <felipeizquierdo47@gmail.com>
2023-05-16 17:27:21 -0500

    Create colliders for limit

---
+++ Assets/Scripts/WayAdvise.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayAdvise : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public UnityEngine.UI.Text timeText;
+    bool onWay = true;
+    bool message;
+    public GameObject panelWay;
+
+    public GameObject panelTimer;
+    public float timer = 5;
+
+    private void FixedUpdate()
+    {
+        if (!onWay)
+        {
+            timer -= Time.deltaTime;
+
+            timeText.text = timer.ToString("F2");
+
+            if (timer <= 3.5f && timer > 2)
+            {
+                timeText.color = Color.yellow;
+            }
+            else if (timer <= 2f)
+            {
+                timeText.color = Color.red;
+            }
+
+            if (timer <= 0)
+            {
+                timer = 0;
+
+
+                onWay = true;
+                panelTimer.SetActive(false);
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Limit")
+        {
+            panelWay.SetActive(true);
+
+            panelTimer.SetActive(true);
+
+            timer = 5;
+            timeText.color = Color.white;
+
+            onWay = false;
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "Limit")
+        {
+            Debug.Log("Alerta salgase de esta zona");
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Limit")
+        {
+            onWay = true;
+
+            panelTimer.SetActive(false);
+        }
+    }
+}
