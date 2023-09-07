d4acf9296a84699b9cc4e4710641709dcdc5a471
FelipeIzq28 <felipeizquierdo47@gmail.com>
2023-09-07 12:09:28 -0500

    Create new script for select player into scene

---
+++ Assets/Resources/Personajes Principales/PlayerInSelector.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInSelector : MonoBehaviour
+{
+    [SerializeField] GameObject[] players;
+    [SerializeField] UserData userData;
+
+    private void Awake()
+    {
+        if (int.TryParse(userData.PlayerSelected, out int result))
+        {
+            PlayerEnabled(result);
+            Debug.Log("Seleccionando personaje " +  result);
+        }
+        else
+        {
+            Debug.Log("El valor almacenado en User Data no es un número");
+        }
+    }
+    void PlayerEnabled(int index)
+    {
+        foreach (var item in players)
+        {
+            item.SetActive(true);
+        }
+
+        players[index].SetActive(true);
+    }
+}
