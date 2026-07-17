0fb80985d2465a082feb2271527322787049f69e
Juanda999 <juanda999games@gmail.com>
2026-07-17 12:05:03 -0500

    Add canvas app info with fps display

--- Assets/Login/script/AppVersionDisplay.cs
+++ Assets/Login/script/AppVersionDisplay.cs
@@ -1,15 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-
 public class AppVersionDisplay : MonoBehaviour
 {
+    TextMeshProUGUI fpsText;
+    private float timer = 0f;
+    private int frames = 0;
+    private float fps = 0f;
     // Start is called before the first frame update
     void Start()
     {
         DontDestroyOnLoad(gameObject.transform.parent);
-        GetComponent<TMPro.TextMeshProUGUI>().text = Application.productName + " -" + Application.version;
+        fpsText = GetComponent<TMPro.TextMeshProUGUI>();
+        fpsText.text = Application.productName + " -" + Application.version;
     }
 
-    
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        frames++;
+
+        // Cada segundo, calculamos el FPS y lo mostramos en pantalla
+        if (timer >= 1f)
+        {
+            fps = frames / timer;
+            fpsText.text = $"{Application.productName} -{Application.version},   {fps:0.} FPS";
+
+            // Reiniciamos el contador
+            timer = 0f;
+            frames = 0;
+        }
+
+    }
+
+
 }
